66c4d81a66206f93c2030e6e480da320538f52be
Pablo Cisneros <cisnerosrojas@icloud.com>
2026-05-01 21:11:52 -0600

    beginning turning into a PWA

--- js/index.js
+++ js/index.js
@@ -1,5 +1,16 @@
 import {loadTheme} from "./theme.js"
 
-
+// In your main.js or inline in HTML
+if ('serviceWorker' in navigator) {
+  window.addEventListener('load', () => {
+    navigator.serviceWorker.register('/service-worker.js')
+      .then(registration => {
+        console.log('ServiceWorker registered successfully:', registration.scope);
+      })
+      .catch(error => {
+        console.log('ServiceWorker registration failed:', error);
+      });
+  });
+}
 
 loadTheme()
