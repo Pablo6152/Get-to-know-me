bb9b8190b695e42d8182c7d2870c7f6ff6e71145
Pablo Cisneros <cisnerosrojas@icloud.com>
2026-05-12 16:17:01 -0600

    Modified the way the main app is rendered

--- js/pages/app.js
+++ js/pages/app.js
@@ -3,9 +3,8 @@ export let app = `
     <div class="item-container">
 
             <div class="item">
-                <h2 class="item-title<">Drafts that meant something to me in the past.</h2>
+                <h2 class="item-title">Drafts that meant something to me in the past.</h2>
                 <p class="item-description">Be careful, I get really personal in some of these</p>
-
             </div>
             
         </div>   
